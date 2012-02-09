8fbf61d122e293353423c6eeb454d90b3afb061b
Ethan Turkeltaub <ethan.turkeltaub@gmail.com>
2012-02-09 15:52:29 -0500

    Added all of the skeletons for the classes.

---
+++ Hand.java
@@ -0,0 +1,33 @@
+
+/**
+ * Write a description of class Hand here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Hand
+{
+	// instance variables - replace the example below with your own
+	private int x;
+
+	/**
+	 * Constructor for objects of class Hand
+	 */
+	public Hand()
+	{
+		// initialise instance variables
+		x = 0;
+	}
+
+	/**
+	 * An example of a method - replace this comment with your own
+	 * 
+	 * @param  y   a sample parameter for a method
+	 * @return	 the sum of x and y 
+	 */
+	public int sampleMethod(int y)
+	{
+		// put your code here
+		return x + y;
+	}
+}
